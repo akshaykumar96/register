08d378c1a8143863672cb9f8059c78cf5c0c7f0f
akshay kumar <akshaykv1tbm@gmail.com>
2018-08-30 10:44:33 +0530

    Update RegisterApplication.java

--- src/main/java/com/multiplex/register/RegisterApplication.java
+++ src/main/java/com/multiplex/register/RegisterApplication.java
@@ -2,11 +2,9 @@ package com.multiplex.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@DataMongoTest
 @EnableMongoRepositories
 @SpringBootApplication
 public class RegisterApplication {
